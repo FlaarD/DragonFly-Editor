5535a195947f4ed836ca49f90c368d81b6941d34
Chresys <faujoux@gmail.com>
2015-08-24 22:32:21 +0200

    Basics for files upload

--- application/Controller.php
+++ application/Controller.php
@@ -17,5 +17,28 @@ class Controller {
         $this->_view = new View('index');
         $this->_view->assign('person', 'Fly');
     }
+    
+    public function uploadAction() {
+        $this->_view = new View('upload');
+        $this->_view->assign('target',BASE_URL.'/verify');
+    }
+    
+    public function verifyAction() {
+        if (isset($_POST['type']) && isset($_FILES['uploaded']['tmp_name'])) {
+            session_start();
+            $json = file_get_contents($_FILES['uploaded']['tmp_name']);
+            if ($_POST['type'] === 'properties') {
+                $json = explode('"itemSets":',$json);
+                $json = explode('KEY_BINDINGS',$json[1]);
+                $json = substr('{"itemSets":'.$json[0],0,strlen('{"itemSets":'.$json[0])-14);
+            }
+            $values = json_decode($json, true);
+            $_SESSION['set'] = $values;
+            header('Location: '.BASE_URL);
+            exit();
+        }
+        header('Location: '.BASE_URL);
+        exit();
+    }
 
 }
