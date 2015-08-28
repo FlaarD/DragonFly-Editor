7eaf63178732639ce4ec11c68375e0336b032fba
Chresys <faujoux.ext@orange.fr>
2015-08-28 12:36:31 +0200

    VC ameliorations, specifically head stuff

--- application/IndexController.php
+++ application/IndexController.php
@@ -9,15 +9,24 @@ require_once PATH.'/application/Controller.php';
 class IndexController extends Controller {
     
     public function blankAction() {
-        $this->_view = new View('blank');
+        $this->_view->setTitle('DragonFly Editor - THE Blank Page');
+        $this->_view->setDescription ('WOW, so much blank !');
     }
     
     public function indexAction() {
-        $this->_view = new View('index');
+        $this->_view->setTitle('DragonFly Editor');
+        $this->_view->setDescription('DragonFly Editor, the main page !');
+        $this->_view->addCss('/css/index.css');
     }
     
 	public function editorAction() {
-		$this->_view = new View('editor');
+        $this->_view->setTitle('DragonFly Editor');
+        $this->_view->setDescription('DragonFly Editor, the editor page !');
+        $this->_view->addCss('/css/editor.css');
 	}
+    
+    public function selectorAction() {
+        $this->_view->setTitle('DragonFly Editor - Selector');
+    }
 
 }
