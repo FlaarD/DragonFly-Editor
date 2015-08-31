7badf178fe65052d93134e48f5a53fcbf0d6d060
Flaar <aetienne.work@gmail.com>
2015-09-01 00:40:09 +0200

    end of classification in PHP upgrade of JS

--- public/js/editor.js
+++ public/js/editor.js
@@ -11,7 +11,26 @@ function show_description(id){
 }
 
 function page_loaded(){
-	
+	$(".TwistedTreeline").hide();
+	$(".HowlingAbyss").hide();
+}
+
+function show_map(chosen_class){
+	switch(chosen_class){
+		case 1 :
+			$(".image_item").hide();
+			$(".OriginalSummonersRift").hide();
+			$(".SummonersRift").hide();
+		break;
+		case 2 :
+			$(".image_item").show();
+			$(".TwistedTreeline").hide();
+		break;
+		case 3 :
+			$(".image_item").show();
+			$(".HowlingAbyss").hide();
+		break;
+	}
 }
 
 $(document).ready(function() {
