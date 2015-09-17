645fba2f416f4722e32917f9f96e416bc4706870
FlaarD <aetienne.work@gmail.com>
2015-09-17 17:37:12 +0200

    Upgrade for the sets of items. Add, +Block and Remove button added. changes on the css for the clarity.  Few other changes.

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,8 +1,13 @@
 function select(id) {
 	$(".selected").removeClass("selected");
-	$(".selected_item_description").hide();
+	$(".selectedItemDescription").hide();
 	$(event.target).addClass("selected");
 	$("#select_"+id).show();
+}
+
+function selectBlock(){
+	$(".selectedBlock").removeClass("selectedBlock");
+	$(event.target).addClass("selectedBlock");
 }
 
 function hide_description(id) {
@@ -21,18 +26,40 @@ function page_loaded(){
 function show_map(chosen_class){
 	switch(chosen_class){
 		case 1 :
-			$(".image_item").show();
+			$(".imageItem").show();
 			$(".OriginalSummonersRift").hide();
 			$(".SummonersRift").hide();
 		break;
 		case 2 :
-			$(".image_item").show();
+			$(".imageItem").show();
 			$(".TwistedTreeline").hide();
 		break;
 		case 3 :
-			$(".image_item").show();
+			$(".imageItem").show();
 			$(".HowlingAbyss").hide();
 		break;
+	}
+}
+
+function add_set_item(){
+	if ($('.selectedBlock').length !==0 && $('.selected').length !==0){
+		alert("Item ajouté en théorie");
+		$('.selectedBlock').html($('.selectedBlock').html() + $('.selected').parent().html());
+		//$('.selected').html()
+	}
+	else {
+		alert("Non trouvé");
+	}
+}
+
+function add_set_block(){
+	//alert("Adding the object YEAH !");
+	$('#sets_zone').html($('#sets_zone').html() + '<div class="setBlock" onclick="selectBlock()"></div>');
+}
+
+function delete_selected(){
+	if ($('.selectedBlock')!==0){
+		$('.selectedBlock').remove();
 	}
 }
 
